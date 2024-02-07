2a552e53064eafd4ac27ed768865098a5c094218
ekanata14 <ekanata1411@gmail.com>
2024-02-07 10:58:37 +0800

    Update: add background image on our program and add some effects in some elements

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Button = ({ text, link }) => {
   return (
     <Link to={link}>
-      <button className="w-30 mt-5 bg-lightBrown px-16 py-3 rounded-md flex justify-center font-poppins font-bold text-black">
+      <button className="w-30 mt-5 bg-lightBrown px-16 py-3 rounded-md flex justify-center font-poppins font-bold text-black hover:scale-125 transition-all duration-300 hover:shadow-2xl">
         {text}
       </button>
     </Link>
--- src/components/OurProgram.jsx
+++ src/components/OurProgram.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import heroImg from "../assets/hero_img.jpg";
+import baksos from "../assets/baksos.jpg";
 
 const OurProgram = () => {
   return (
     <div className="w-full mt-5 font-poppins h-[100%]">
       <div className="container mx-auto grid grid-cols-1 xl:grid-cols-3 gap-y-16 justify-items-center h-full font-mono mt-10 p-10 text-center text-white">
-        <div className="h-96 w-[350px] xl:w-96 bg-blue rounded-xl flex justify-center items-center shadow-2xl">
+        <div className="h-96 w-[350px] xl:w-96 bg-blue rounded-xl flex justify-center items-center shadow-md cursor-pointer hover:shadow-2xl hover:scale-110 transition-all duration-300">
           <h1 className="text-3xl font-bold">Ramadhan Bazzar</h1>
         </div>
-        <div className="h-96 w-[350px] xl:w-96 bg-blue rounded-xl flex justify-center items-center shadow-2xl">
+        <div className="h-96 w-[350px] xl:w-96 bg-cover bg-center rounded-xl flex justify-center items-center shadow-md cursor-pointer hover:shadow-2xl hover:scale-110 transition-all duration-300" style={{backgroundImage: `url(${baksos})`}}>
           <h1 className="text-3xl font-bold">Bakti Sosial</h1>
         </div>
-        <div className="h-96 w-[350px] xl:w-96 bg-blue rounded-xl flex justify-center items-center shadow-2xl">
+        <div className="h-96 w-[350px] xl:w-96 bg-blue rounded-xl flex justify-center items-center shadow-md cursor-pointer hover:shadow-2xl hover:scale-110 transition-all duration-300">
           <h1 className="text-3xl font-bold">Islamic Day Event</h1>
         </div>
       </div>
