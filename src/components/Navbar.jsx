8a56acbe254cbc6441260f17f37a925c6fcffb38
ekanata14 <ekanata1411@gmail.com>
2025-03-17 02:33:44 +0800

    feat: update landing page and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,23 +6,23 @@ const Navbar = () => {
     <div className="w-full shadow-md font-poppins">
       <div className="container mx-auto flex justify-center xl:justify-between h-28 text-black">
         <img src={logohrm} alt="hrm-logo" className="max-w-32" />
-          <ul className="hidden xl:flex h-full">
-            <a href="#aboutUs">
+        <ul className="hidden xl:flex h-full">
+          <a href="#aboutUs">
             <li className="h-full px-10 text-xl flex items-center hover:text-slate-50 hover:bg-blue hover:cursor-pointer font-bold transition-all ease-in duration-300">
               Tentang Kami
             </li>
-            </a>
-            <a href="#ourProgram">
+          </a>
+          <a href="#ourProgram">
             <li className="h-full px-10 text-xl flex items-center hover:text-slate-50 hover:bg-blue hover:cursor-pointer font-bold transition-all ease-in duration-300">
               Program Kami
             </li>
-            </a>
-            <a href="https://forms.gle/MWyqe52NBtC8Loet8">
+          </a>
+          {/* <a href="https://forms.gle/MWyqe52NBtC8Loet8">
             <li className="h-full px-10 text-xl flex items-center hover:text-slate-50 bg-lightBrown hover:bg-blue hover:cursor-pointer font-bold transition-all ease-in duration-300">
               DAFTAR
             </li>
-            </a>
-          </ul>
+          </a> */}
+        </ul>
       </div>
     </div>
   );
